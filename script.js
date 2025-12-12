544cd7f9b61b6027366b90305731ecce0aa9db48
themeg-byte <libaypota01@gmail.com>
2025-12-13 02:09:40 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const inputType = document.getElementById("inputType");
+const outputType = document.getElementById("outputType");
+const inputValue = document.getElementById("inputValue");
+const outputValue = document.getElementById("outputValue");
+
+function convert() {
+  let fromBase = Number(inputType.value);
+  let toBase = Number(outputType.value);
+  let value = inputValue.value;
+
+  if (fromBase === 2 && !/^[01]+$/.test(value)) {
+    outputValue.value = "Invalid!";
+    return;
+  }
+
+  let decimalNumber = parseInt(value, fromBase);
+
+  if (isNaN(decimalNumber)) {
+    outputValue.value = "Invalid!";
+    return;
+  }
+
+  outputValue.value = decimalNumber.toString(toBase).toUpperCase();
+}
+
+function clr() {
+  inputValue.value = "";
+  outputValue.value = "";
+}
