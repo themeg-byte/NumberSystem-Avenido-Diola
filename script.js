6f8541c7e42383a97a0be4fee6c7a548c87f6f93
themeg-byte <libaypota01@gmail.com>
2025-12-13 03:12:04 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,24 +8,37 @@ function convert() {
   let toBase = Number(outputType.value);
   let value = inputValue.value;
 
- if (fromBase === 2 && !/^[01]+$/.test(value)) {
-    window.alert("Invalid! Must input only 0,1");
+  if (fromBase === 2 && !/^[01]+$/.test(value)) {
+    alert("Invalid! Must input only 0,1");
     return;
-}
-    if (fromBase === 8 && !/^[01234567]+$/.test(value)) {
-    window.alert("Invalid! Must input only 0-7");
+  }
+
+  if (fromBase === 8 && !/^[01234567]+$/.test(value)) {
+    alert("Invalid! Must input only 0-7");
     return;
-}
-    if (fromBase === 16 && !/^[0123456789AaBbCcDdEeFf]+$/.test(value)) {
-    window.alert("Invalid! Must input only 0-9 and A-F");
+  }
+
+  if (fromBase === 16 && !/^[0123456789AaBbCcDdEeFf]+$/.test(value)) {
+    alert("Invalid! Must input only 0-9 and A-F");
     return;
-}
-    if (fromBase === 10 && !/^[0123456789]+$/.test(value)) {
-    window.alert("Invalid! Must input only 0-9");
+  }
+
+  if (fromBase === 10 && !/^[0123456789]+$/.test(value)) {
+    alert("Invalid! Must input only 0-9");
     return;
+  }
+
+  let decimalNumber = parseInt(value, fromBase);
+
+  if (isNaN(decimalNumber)) {
+    outputValue.value = "Invalid!";
+    return;
+  }
+
+  outputValue.value = decimalNumber.toString(toBase).toUpperCase();
 }
+
 function clr() {
-  inputValue.value = "";
-  outputValue.value = "";
-}
+  document.getElementById("inputValue").value = "";
+  document.getElementById("outputValue").value = "";
 }
