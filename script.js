e8ddcab54868c5dfe52d6bd8993d7a63de87d2d9
themeg-byte <libaypota01@gmail.com>
2025-12-13 02:40:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -8,25 +8,18 @@ function convert() {
   let toBase = Number(outputType.value);
   let value = inputValue.value;
 
-  if (fromBase === 2 && !/^[01]+$/.test(value)) {
-    outputValue.value = "Invalid!";
+ if (fromBase === 2 && !/^[01]+$/.test(value)) {
+    alert("Invalid! Must input only 0,1");
     return;
-  }
-  if (fromBase === 8 && !/^[01234567]+$/.test(value)) {
-    outputValue.value = "Invalid!";
+}
+    if (fromBase === 8 && !/^[01234567]+$/.test(value)) {
+    alert("Invalid! Must input only 0-7");
     return;
 }
     if (fromBase === 16 && !/^[0123456789AaBbCcDdEeFf]+$/.test(value)) {
-    outputValue.value = "Invalid!";
+    alert("Invalid! Must input only 0-9 and A-F");
     return;
 }
-
-  let decimalNumber = parseInt(value, fromBase);
-
-  if (isNaN(decimalNumber)) {
-    outputValue.value = "Invalid!";
-    return;
-  }
 
   outputValue.value = decimalNumber.toString(toBase).toUpperCase();
 }
@@ -35,5 +28,6 @@ function clr() {
   inputValue.value = "";
   outputValue.value = "";
 }
+
 
 
