2feffa741c347d0daafd2c89d91574fde06992c8
themeg-byte <libaypota01@gmail.com>
2025-12-13 02:49:01 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,25 +9,26 @@ function convert() {
   let value = inputValue.value;
 
  if (fromBase === 2 && !/^[01]+$/.test(value)) {
-    alert("Invalid! Must input only 0,1");
+    window.alert("Invalid! Must input only 0,1");
     return;
 }
     if (fromBase === 8 && !/^[01234567]+$/.test(value)) {
-    alert("Invalid! Must input only 0-7");
+    window.alert("Invalid! Must input only 0-7");
     return;
 }
     if (fromBase === 16 && !/^[0123456789AaBbCcDdEeFf]+$/.test(value)) {
-    alert("Invalid! Must input only 0-9 and A-F");
+    window.alert("Invalid! Must input only 0-9 and A-F");
     return;
 }
     if (fromBase === 10 && !/^[0123456789]+$/.test(value)) {
-    alert("Invalid! Must input only 0-9");
+    window.alert("Invalid! Must input only 0-9");
     return;
 }
 function clr() {
   inputValue.value = "";
   outputValue.value = "";
 }
+
 
 
 
